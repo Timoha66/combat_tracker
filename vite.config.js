b29b18c726c6bf7e8c15e16cf3bd9538e4fc2a0c
Timoha66 <timonakaartem666@gmail.com>
2026-05-19 02:26:50 +0300

    repo name changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ⚠️ Измени 'dm-tracker' на имя твоего GitHub репозитория
 export default defineConfig({
   plugins: [react()],
-  base: '/dm-tracker/',
+  base: '/combat_tracker/',
 })
