93707dfd59f675465533a3c0d96b9e88f08afdc3
Timoha66 <timonakaartem666@gmail.com>
2026-05-27 18:37:19 +0300

    seedloader fix
    Seed load deleted for NPCs and Locations

--- src/data/locationsSeedLoader.js
+++
@@ -1,3 +0,0 @@
-export async function loadLocationsSeedIfEmpty() {
-  // seed отключён
-}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { loadSeedIfEmpty } from './data/seedLoader'
-import { loadLocationsSeedIfEmpty } from './data/locationsSeedLoader'
-import { loadNpcSeedIfEmpty } from './data/npcSeedLoader'
 
 loadSeedIfEmpty()
-loadLocationsSeedIfEmpty()
-loadNpcSeedIfEmpty()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
